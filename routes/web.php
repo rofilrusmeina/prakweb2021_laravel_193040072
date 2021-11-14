edc788fa553d02fac1d2893d105bc15ec1d54a8a
rofilrusmeina <58686996+rofilrusmeina@users.noreply.github.com>
2021-11-14 12:02:11 +0700

    Database, Migration $ Eloquent (Tugas 6)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,55 +32,5 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Rofil Rusmeina",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, animi aspernatur. Necessitatibus eos nostrum architecto quibusdam sed suscipit quasi reprehenderit maxime doloribus non aspernatur ipsa sequi, laboriosam unde quos saepe. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum qui officia culpa quasi saepe, eos numquam, error recusandae doloribus fugit quos! Voluptatum deleniti corrupti maiores animi eligendi quidem, explicabo quisquam."
-        ],
-        [
-            "title" => "Judul post Kedua",
-            "slug" => "judul-post-keedua",
-            "author" => "Doddy Ferdiansyah",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, animi aspernatur. Necessitatibus eos nostrum architecto quibusdam sed suscipit quasi reprehenderit maxime doloribus non aspernatur ipsa sequi, laboriosam unde quos saepe. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum qui officia culpa quasi saepe, eos numquam, error recusandae doloribus fugit quos! Voluptatum deleniti corrupti maiores animi eligendi quidem, explicabo quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum qui officia culpa quasi saepe, eos numquam, error recusandae doloribus fugit quos! Voluptatum deleniti corrupti maiores animi eligendi quidem, explicabo quisquam."
-        ]
-        ];
-    return view('posts', [
-        "title" => "posts",
-        "posts" => $blog_posts
-    ]);
-}); 
-
-
-// Halaman single post
-
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Rofil Rusmeina",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, animi aspernatur. Necessitatibus eos nostrum architecto quibusdam sed suscipit quasi reprehenderit maxime doloribus non aspernatur ipsa sequi, laboriosam unde quos saepe. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum qui officia culpa quasi saepe, eos numquam, error recusandae doloribus fugit quos! Voluptatum deleniti corrupti maiores animi eligendi quidem, explicabo quisquam."
-        ],
-        [
-            "title" => "Judul post Kedua",
-            "slug" => "judul-post-keedua",
-            "author" => "Doddy Ferdiansyah",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, animi aspernatur. Necessitatibus eos nostrum architecto quibusdam sed suscipit quasi reprehenderit maxime doloribus non aspernatur ipsa sequi, laboriosam unde quos saepe. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum qui officia culpa quasi saepe, eos numquam, error recusandae doloribus fugit quos! Voluptatum deleniti corrupti maiores animi eligendi quidem, explicabo quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum qui officia culpa quasi saepe, eos numquam, error recusandae doloribus fugit quos! Voluptatum deleniti corrupti maiores animi eligendi quidem, explicabo quisquam."
-        ]
-        ];
-
-        $new_post = [];
-        foreach($blog_posts as $post) {
-            if($post["slug"] === $slug) {
-                $new_post = $post;
-            }
-        }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']); 
+Route::get('posts/{slug}', [PostController::class, 'show']);
